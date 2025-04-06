7253a29d34c9355c67562e9a05c75fe1bf667d08
Danish Ali Umar <danishaliumar4@gmail.com>
2025-04-06 17:49:24 +0500

    init npm

---
+++ hello-world/hello.js
@@ -0,0 +1,8 @@
+// javascript pre defined methods does not work in node.js
+// console.log(window); // window is not defined in node.js
+// but you can use globalThis
+// console.log(globalThis); // globalThis is the global object in node.js
+// console.log(document); // document is not defined in node.js
+// console.log(alert('Hey')); // alert is not defined in node.js
+// but you can use console.log to print to the console
+// console.log('hello world'); // this will print to the console
